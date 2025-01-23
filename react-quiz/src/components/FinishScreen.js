3fc53b501ec097d137bbbb8c9a31076b83b1cca3
akashrathore94 <akashrathour2011@gmail.com>
2025-01-23 15:45:37 +0530

    added react quiz project

---
+++ react-quiz/src/components/FinishScreen.js
@@ -0,0 +1,31 @@
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+  let emoji;
+
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🤩";
+  if (percentage >= 50 && percentage < 60) emoji = "🥉";
+  if (percentage > 0 && percentage < 650) emoji = "🧳";
+  if (percentage === 0) emoji = "🤦";
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>
+        You scored <strong>{points} </strong>
+        out of {maxPossiblePoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore} Points)</p>
+
+      <button
+        className="btn btn-ui"
+        onClick={() => {
+          dispatch({ type: "restart" });
+        }}
+      >
+        Restart Quiz
+      </button>
+    </>
+  );
+}
+
+export default FinishScreen;
