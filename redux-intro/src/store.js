6b2872850d88b8531766d847148ce5e19b73d2d4
akashrathore94 <akashrathour2011@gmail.com>
2025-02-10 10:23:06 +0530

    store initiated

---
+++ redux-intro/src/store.js
@@ -0,0 +1,37 @@
+import { createStore } from "redux";
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        balance: state.balance + action.payload.loan,
+        loan: action.payload.loan,
+        loanPurpose: action.payload.loanPurpose,
+      };
+
+    case "account/payLoan":
+      return {
+        ...state,
+        balance: state.balance - state.loan,
+        loan: 0,
+        loanPurpose: "",
+      };
+
+    default:
+      return state;
+  }
+}
